3adb3f338548cf4fca286e26681299cdcae22d02
Pluseon7 <y02121148s@gmail.com>
2018-04-20 15:39:58 +0800

    argv

--- crawler.py
+++ crawler.py
@@ -1,10 +1,15 @@
 
 
 from icrawler.builtin import GoogleImageCrawler
+import sys
 
+
+search_content = str(sys.argv[1])
+out_put_dir = str(sys.argv[2])
+print(out_put_dir)
 google_crawler = GoogleImageCrawler(
-    feeder_threads=15,
-    parser_threads=15,
+    feeder_threads=1,
+    parser_threads=1,
     downloader_threads=100,
-    storage={'root_dir': './1410332003'})
-google_crawler.crawl(keyword='doona bea',max_num=1000, file_idx_offset=0, min_size=None)
+    storage={'root_dir': './'+ out_put_dir})
+google_crawler.crawl(keyword=search_content,max_num=1000, file_idx_offset=0, min_size=None)
