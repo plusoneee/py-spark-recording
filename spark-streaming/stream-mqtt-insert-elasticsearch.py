6949785148066acfcad9484521b189d309a9f2e6
Jiayi  Liao <y02121148s@gmail.com>
2018-09-12 13:59:12 +0800

    Update stream-mqtt-insert-elasticsearch.py
    add insert timestamp

--- spark-streaming/stream-mqtt-insert-elasticsearch.py
+++ spark-streaming/stream-mqtt-insert-elasticsearch.py
@@ -11,19 +11,18 @@ from elasticsearch import Elasticsearch
 from datetime import datetime
 import json
 
-
 def send_streaming_to_Elastic(data):
     es = Elasticsearch()
     doc = json.loads(data)
-    stat_es = es.index(index="machan-laser", doc_type="test-type", body=doc)
-    print(type(doc))
-    print(doc)
+    doc["timestamp"] = datetime.today().strftime('%Y-%m-%d %H:%m:%s')
+    stat_es = es.index(index="fem-currents", doc_type="test-type", body=doc)
+    #print(doc)
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         print >> sys.stderr, "Usage: mqtt_wordcount.py <broker url> <topic>"
         exit(-1)
     sc = SparkContext(appName="PythonStreamingMQTT_ELK")
-    ssc = StreamingContext(sc, 1) 
+    ssc = StreamingContext(sc, 1)
     brokerUrl ='tcp://'+sys.argv[1]
     topic = sys.argv[2]
     lines = MQTTUtils.createStream(ssc, brokerUrl, topic)
@@ -32,4 +31,3 @@ if __name__ == "__main__":
     mqtt_get_str.foreachRDD(lambda rdd: rdd.foreach(send_streaming_to_Elastic))
     ssc.start()
     ssc.awaitTermination()
-
